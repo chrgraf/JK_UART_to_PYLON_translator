cdfeaddfacaaac05302111021657bd893e9a4e38
chrgraf <cgraf@juniper.net>
2023-12-21 13:06:29 +0100

    adding mqtt functions

--- get_files.sh
+++ get_files.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-files=(alien_master.service basic_ringbuf.py README.md requirements.txt jk_pylon.service jk_pylon_can.py pylon_CAN_210124.dbc)
+files=(my_mqtt.py alien_master.service basic_ringbuf.py README.md requirements.txt jk_pylon.service jk_pylon_can.py pylon_CAN_210124.dbc)
 
 for i in "${files[@]}"
   do scp behn@192.168.178.144:jk_pylon/$i .
---
+++ my_mqtt.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+
+import random
+import time
+
+from paho.mqtt import client as mqtt_client
+
+
+broker = '192.168.178.116'
+port = 1883
+# generate client ID with pub prefix randomly
+client_id = f'python-mqtt-{random.randint(0, 1000)}'
+username = ''
+password = ''
+
+def connect_mqtt():
+    def on_connect(client, userdata, flags, rc):
+        if rc == 0:
+            print("Connected to MQTT Broker!")
+        else:
+            print("Failed to connect, return code %d\n", rc)
+
+    client = mqtt_client.Client(client_id)
+    client.username_pw_set(username, password)
+    client.on_connect = on_connect
+    client.connect(broker, port)
+    return client
+
+
+def publish(client,topic,message):
+        result = client.publish(topic, message)
+        # result: [0, 1]
+        status = result[0]
+        if status == 0:
+            print(f"Send `{message}` to topic `{topic}`")
+        else:
+            print(f"Failed to send message to topic {topic}")
+
+
+def run():
+    client = connect_mqtt()
+    client.loop_start()
+    publish(client)
+
+
+if __name__ == '__main__':
+    run()
+
